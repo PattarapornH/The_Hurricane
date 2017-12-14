ecbeb55bfc969a4fe82c15b9136fd21b99cb335e
PattarapornH <pataraporn.han@gmail.com>
2017-12-14 21:03:46 +0700

    random

--- core/src/com/mygdx/game/World.java
+++ core/src/com/mygdx/game/World.java
@@ -5,35 +5,29 @@ import com.badlogic.gdx.Gdx;
 import java.util.Random;
 
 public class World {
-    //private float count;
+    private float count = 0;
     private float totalTime = 60;
-    private float deltaTime ;
+    private float deltaTime;
     private int sec;
     private int randItem;
     private int randX;
+
     Random random = new Random();
 
+    WorldRenderer worldRenderer;
+
     public void World() {
-
+        //this.worldRenderer = new WorldRenderer();
     }
 
-    public void Timer(){
+    public void Timer() {
         deltaTime = Gdx.graphics.getDeltaTime();
+        sec = (int) (totalTime % 60);
         totalTime -= deltaTime;
-        sec = (int)(totalTime % 60);
-        System.out.println(sec);
     }
 
-    public int RandomItem(){
-        if(sec - (int)(totalTime%60) > 1){
-            randItem = random.nextInt(2);
-        }
-        return randItem;
-    }
-    public int RandomXPosition(){
-        if(sec - (int)(totalTime%60) > 1){
-            randX = random.nextInt(2);
-        }
-        return randX;
+    public int random(int rand) {
+        rand = random.nextInt(2);
+        return rand;
     }
 }
